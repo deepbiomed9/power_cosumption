e9ed597d555107b16ac14c3f6173b532f28235cc
deep pandya <deep.pandya@wchn.org>
2016-10-04 12:24:40 -0400

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,1 @@
+hist(subpowerData$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
---
+++ plot2.R
@@ -0,0 +1,1 @@
+plot(subpowerData$powerTime, subpowerData$Global_active_power, type="l", col="black", xlab="", ylab="Global Active Power (kilowatts)")
